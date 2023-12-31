59ceb5592749098025c9036d8351636db875635a
JawaraGordon <jawaragordon@icloud.com>
2023-12-31 11:26:46 -0500

    Complete Storybook config: Completed init setup of storybook, added a test button instance

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -6,40 +6,48 @@ export default {
   component: Button,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: 'centered',
+    layout: 'centered'
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+    backgroundColor: { control: 'color' }
+  }
 };
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary = {
   args: {
     primary: true,
-    label: 'Button',
-  },
+    label: 'Button'
+  }
 };
 
 export const Secondary = {
   args: {
-    label: 'Button',
-  },
+    label: 'Button'
+  }
 };
 
 export const Large = {
   args: {
     size: 'large',
-    label: 'Button',
-  },
+    label: 'Button'
+  }
 };
 
 export const Small = {
   args: {
     size: 'small',
-    label: 'Button',
-  },
+    label: 'Button'
+  }
+};
+
+export const Warning = {
+  args: {
+    primary: true,
+    label: 'Delete now',
+    backgroundColor: 'red'
+  }
 };
