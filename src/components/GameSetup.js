7b03f2d00d678a59037207a3de3cd94e01c0f202
JawaraGordon <jawaragordon@icloud.com>
2023-12-31 18:26:50 -0500

    Add input sliders to each player div: Created sliders on each player div to control intensity

--- src/components/GameSetup.js
+++ src/components/GameSetup.js
@@ -1,13 +1,23 @@
 const GameSetup = () => {
   return (
-    <div className="bg-gradient-to-r from-purple-400 to-blue-500 flex flex-col justify-center items-center h-screen">
-
-<h1 className="text-white text-4xl font-bold leading-none mb-8">
-          Game Setup
-        </h1>
+<div className="bg-gradient-to-r from-purple-400 to-blue-500 flex flex-col justify-center items-center h-screen">
+  <h1 className="text-white text-4xl font-bold leading-none mb-8">
+    Game Setup
+  </h1>
+  <div className="flex justify-center w-full px-28">
+    <div className="flex justify-between space-x-48">
+      <div className="bg-white p-8">
+        <h2>Player 1 Intensity</h2>
+        <input type="range" min="1" max="100" />
+      </div>
+      <div className="bg-white p-8">
+        <h2>Player 2 Intensity</h2>
+        <input type="range" min="1" max="100" />
+      </div>
     </div>
-  )
-}
-
+  </div>
+</div>
+  );
+};
 
 export default GameSetup;
