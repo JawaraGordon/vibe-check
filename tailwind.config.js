119dc7100c81e4a90663235d9065c94774eb691c
JawaraGordon <jawaragordon@icloud.com>
2023-12-31 00:06:38 -0500

    Add storybook for UI Installed and configured storybook to start buildig project UI library

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.html',
+    './src/**/*.js',
+    './src/**/*.jsx',
+    './src/**/*.ts',
+    './src/**/*.tsx',
+    // other paths to your templates...
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
