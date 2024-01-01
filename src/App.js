14f92ceda211b04ce1a1bd36a2c842b9f74b9022
JawaraGordon <jawaragordon@icloud.com>
2024-01-01 10:48:11 -0500

    Create a GamePlay component: Added a game play component, story and test with two input divs for players and an audio element

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import './index.css';
 import Main from './components/Main';
 import GameSetup from './components/GameSetup';
+import GamePlay from './components/GamePlay';
 
 function App() {
   return (
     <>
       <Main />
       <GameSetup />
+      <GamePlay />
     </>
   );
 }
---
+++ src/components/GamePlay.js
@@ -0,0 +1,28 @@
+const GamePlay = () => {
+  return (
+<div className="bg-gradient-to-r from-purple-400 to-blue-500 flex flex-col justify-center items-center h-screen">
+  <h1 className="text-white text-4xl font-bold leading-none mb-8">
+    Game Play
+  </h1>
+  <div className="flex flex-col justify-center items-center w-full px-28">
+    <div className="bg-white p-8 mb-8">
+      <h2>Song Waveform</h2>
+      {/* TODO: create HTML5 audio element and visual display */}
+      <audio src="http://"></audio>
+    </div>
+    <div className="flex justify-between space-x-48">
+      <div className="bg-purple-600 p-8">
+        <h2>Player 1 Input</h2>
+        <input type="text" />
+      </div>
+      <div className="bg-blue-600 p-8">
+        <h2>Player 2 Input</h2>
+        <input type="text" />
+      </div>
+    </div>
+  </div>
+</div>
+  );
+};
+
+export default GamePlay;
