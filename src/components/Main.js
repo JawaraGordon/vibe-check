51118fb05955b927c1a2ba22b7712f91fc5d80ae
JawaraGordon <jawaragordon@icloud.com>
2024-01-07 07:57:06 -0500

    Create a Help screen to show game instructions: Added a modal, css, and copy to allow players to click on a Help button on the Main screen allowing them to learn how to play the game

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,31 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from '../components/common/Button';
 // import Header from '../components/common/Header';
+import Modal from './utils/HelpModal';
 import VibeLogo from '../../src/assets/images/vibe-check-logo.png';
 
 const Main = () => {
+  const [showModal, setShowModal] = useState(false);
   const navigate = useNavigate();
+  const gameInstructions = `Game Start & Setup:
+
+Start your neon-drenched, musical journey by pressing "Start". Set the game's intensity using the slider to influence song tempo and vibe.
+
+Gameplay:
+
+Collaboratively type words matching the song's vibe to score points. Avoid repeating words or using your teammate's words. You have 60 seconds to match or beat the top score for the chosen song!
+
+Game Results & Intensity Selection:
+
+Check your scores and see if you've matched the right vibes. Decide to start a new round or quit. Refine your game experience by adjusting the intensity for the next round based on your previous gameplay.`;
 
   const handleStartClick = () => {
-    navigate('/setup'); 
+    navigate('/setup');
+  };
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
   };
 
   return (
@@ -15,27 +33,29 @@ const Main = () => {
       {/* <Header /> */}
       <div className="bg-gradient-to-r from-purple-400 to-blue-500 flex flex-col justify-center items-center h-screen">
         <div className="mb-8 z-10">
-        <img src={VibeLogo} alt="Vibe Logo" />
+          <img src={VibeLogo} alt="Vibe Logo" />
         </div>
         <div className="flex flex-row justify-evenly items-center w-full max-w-xs">
           <Button label="Start" onClick={handleStartClick} />
-          {/* <Button label="Exit" onClick={() => {}} /> */}
+          <Button label="Help" onClick={toggleModal} />
         </div>
       </div>
-      <div classNAme="area" >
-            <ul class="circles">
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-            </ul>
-    </div >
+      {showModal && <Modal text={gameInstructions} onClose={toggleModal} />}
+
+      <div classNAme="area">
+        <ul class="circles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
     </>
   );
 };
---
+++ src/components/utils/HelpModal.js
@@ -0,0 +1,38 @@
+const HelpModal = ({ onClose }) => {
+  return (
+    <div className="modal-background-help">
+      <div className="modal-content-help">
+        <button className="modal-close-button" onClick={onClose}>
+          X
+        </button>
+        <h2 className="modal-sub-header">Game Start</h2>
+        <p className="modal-text">
+          Begin your musical journey by pressing the "Start button.
+        </p>
+
+        <h2 className="modal-sub-header">Game Setup</h2>
+        <p className="modal-text">
+          Set the game's intensity by using the slider to influence the song
+          tempo and vibe. The combined average of both player's sliders
+          determines the game difficulty
+        </p>
+
+        <h2 className="modal-sub-header">Game Play</h2>
+        <p className="modal-text">
+          Collaboratively type words matching the song's vibe to score points.
+          Avoid repeating words or using your teammate's words. You have 60
+          seconds to match or beat the top score for the chosen song!
+        </p>
+
+        <h2 className="modal-sub-header">Game Results</h2>
+        <p className="modal-text">
+          Check your scores and see if you've matched the right vibes. Decide to
+          start a new round or quit. Refine your game experience by adjusting
+          the intensity for the next round based on your previous game play.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default HelpModal;
